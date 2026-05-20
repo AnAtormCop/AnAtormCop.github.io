96bca55dc553ff842029a28d00a4413b5355c12f
Windows_11DevFan <windowscorporation456@gmail.com>
2026-05-20 23:58:35 +0800

    Create list.js

---
+++ list.js
@@ -0,0 +1,72 @@
+// Hey there! you found this source code!!  lets say... its basically stupid so goodluck
+
+// 1. Global Link properties
+const globalConfigs = {
+    telegram_url: "https://t.me/fevshit"
+};
+
+// 2. Active ROM list database
+const deviceDatabase = [
+    {
+        name: "XOS 15.1.2",
+        codename: "X6525",
+        maintainer: "gray tv head",
+        status: "Official",
+        banner_image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-KdmBCTZpX4l3pPp53V68o3jxpG5xINXZSQ&s",
+        
+        // Button Configuration
+        btn1_text: "Download",
+        download_url: "https://t.me/fevshit/87",
+        
+        btn2_text: "", // Empty string means it completely disappears
+        btn2_url: "https://t.me/fevshit/87" 
+    },
+    {
+        name: "Note 40i Port",
+        codename: "X6525",
+        maintainer: "Seuj123",
+        status: "Unofficial",
+        banner_image_url: "https://i.ytimg.com/vi/3rJyZ1hP9cs/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAHG633GMY3pu4C3FcstXQb-Nu6CA",
+        
+        // Button Configuration
+        btn1_text: "Download",
+        download_url: "https://archive.org/details/super_202602",
+        
+        btn2_text: "Updated Version",
+        btn2_url: "https://t.me/infinix_smart8/53767"
+    }
+];
+
+// 3. Flashing Tools Dataset with OS assignments
+const toolsDatabase = [
+    {
+        name: "Spreadtrum Tools | Massatriof16 ",
+        desc: "Focused on unisoc chips and its free tool!!",
+        url: "https://github.com/Massatriof16/recovery-collections/releases/download/Spd_flash/spreadtrum_flash_V34.zip",
+        os: ["windows"]
+    }
+];
+
+// 4. Project Credits Dataset
+const creditsDatabase = [
+    { 
+        role: "XOS 15 Port", 
+        name: "NotHinPorts", 
+        profile_url: "https://t.me/fevshit"
+    },
+    { 
+        role: "Note 40i Port", 
+        name: "Seuj123", 
+        profile_url: "https://t.me/Seuj123"
+    },
+	{ 
+        role: "Note 40i Updated Version", 
+        name: "vnsannn", 
+        profile_url: "https://t.me/vnsannn"
+    },
+    { 
+        role: "Website Developer", 
+        name: "gray tv head", 
+        profile_url: "https://t.me/squarygray" 
+    }
+];
